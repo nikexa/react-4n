df0a41b62780f2b3f2122412708733f6950626e0
nikoloz <nikusha.tskhvaro@gmail.com>
2025-01-23 02:33:46 +0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ import strawberryCakeCaloriesImage from './images/marwyviCaloris.svg'
 function App() {
   return (
     <>
-      <Card mtavari={pizzaImage} name={"Pepperoni Pizza"} ragac={forPizzaImage} text={"Premium pepperoni and cheese is made with real mozzarella on original <br /> hand-tossed crust."} Calories={pizzaCaloriesImage} price={"$23.90"} oldPrice={"$29.90"}/>
+      <Card mtavari={pizzaImage} name={"Pepperoni Pizza"} ragac={forPizzaImage} text={"Premium pepperoni and cheese is made with real mozzarella on original  hand-tossed crust."} Calories={pizzaCaloriesImage} price={"$23.90"} oldPrice={"$29.90"}/>
       <Card mtavari={tortelliniImage} name={"Tortellini"} ragac={forTortelliniImage} text={"Pasta stuffed with beef and pork topped with your choice of cream sauce with bacon, bolognese or marinara sauce."} Calories={tortelliniCaloriesImage} price={"$17.90"} oldPrice={"$22.90"}/>    
       <Card mtavari={strawberryCakeImage} name={"Strawberry Cake"} ragac={forStrawberryCakeImage} text={"Three layer strawberry dessert is not only beautiful looking, but also delicious! Perfect dessert for spring and summer."} Calories={strawberryCakeCaloriesImage} price={"$13.90"} oldPrice={"$18.90"}/>
     </>
