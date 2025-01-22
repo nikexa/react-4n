97529272bb7634f3d73b2f91defbb86467b1335e
nikoloz <nikusha.tskhvaro@gmail.com>
2025-01-23 02:30:43 +0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import './App.css'
 import Card from './components/__organism/card.jsx'
 
-function App() {
 
+import pizzaImage from './images/pizza.svg'
+import forPizzaImage from './images/forPizza.svg'
+import pizzaCaloriesImage from './images/pizzaCaloris.svg'
+import tortelliniImage from './images/tortenili.svg'
+import forTortelliniImage from './images/forTortelioni.svg'
+import tortelliniCaloriesImage from './images/torteliniCaloris.svg'
+import strawberryCakeImage from './images/marwyvi.svg'
+import forStrawberryCakeImage from './images/forMarwyvi.svg'
+import strawberryCakeCaloriesImage from './images/marwyviCaloris.svg'
+
+function App() {
   return (
     <>
-    <Card mtavari={"./src/images/pizza.svg"} name={"Pepperoni Pizza"} ragac={"src/images/forPizza.svg"} text={"Premium pepperoni and cheese is made with real mozzarella on original <br /> hand-tossed crust."} Calories={"src/images/pizzaCaloris.svg"} price={"$23.90"} oldPrice={"$29.90"}/>
-    <Card mtavari={"./src/images/tortenili.svg"} name={"Tortellini"} ragac={"src/images/forTortelioni.svg"} text={"Pasta stuffed with beef and pork topped with your choice of cream sauce with bacon, bolognese or marinara sauce."} Calories={"src/images/torteliniCaloris.svg"} price={"$17.90"} oldPrice={"$22.90"}/>    
-    <Card mtavari={"./src/images/marwyvi.svg"} name={"Strawberry Cake"} ragac={"src/images/forMarwyvi.svg"} text={"Three layer strawberry dessert is not only beautiful looking, but also delicious! Perfect dessert for spring and summer."} Calories={"src/images/marwyviCaloris.svg"} price={"$13.90"} oldPrice={"$18.90"}/>
+      <Card mtavari={pizzaImage} name={"Pepperoni Pizza"} ragac={forPizzaImage} text={"Premium pepperoni and cheese is made with real mozzarella on original <br /> hand-tossed crust."} Calories={pizzaCaloriesImage} price={"$23.90"} oldPrice={"$29.90"}/>
+      <Card mtavari={tortelliniImage} name={"Tortellini"} ragac={forTortelliniImage} text={"Pasta stuffed with beef and pork topped with your choice of cream sauce with bacon, bolognese or marinara sauce."} Calories={tortelliniCaloriesImage} price={"$17.90"} oldPrice={"$22.90"}/>    
+      <Card mtavari={strawberryCakeImage} name={"Strawberry Cake"} ragac={forStrawberryCakeImage} text={"Three layer strawberry dessert is not only beautiful looking, but also delicious! Perfect dessert for spring and summer."} Calories={strawberryCakeCaloriesImage} price={"$13.90"} oldPrice={"$18.90"}/>
     </>
   )
 }
